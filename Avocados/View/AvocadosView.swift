a19d173afd08f2e03d5a92348c963089f3f123c1
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-28 17:51:33 +0500

    An main image and its title is implemneted in the avocados view screen and work done

--- Avocados/View/AvocadosView.swift
+++ Avocados/View/AvocadosView.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct AvocadosView: View {
     var body: some View {
-        Text("Avocados")
+        VStack {
+            Spacer()
+            
+            Image("avocado")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 240, height: 240, alignment: .center)
+                .shadow(color: Color("ColorBlackTransparentDark"), radius: 12, x: 0, y: 8)
+            
+            Text("Avocados")
+                .font(.system(size: 42, weight: .bold, design: .serif))
+                .foregroundColor(.white)
+                .padding()
+                .shadow(color: Color("ColorBlackTransparentDark"), radius: 4, x: 0, y: 4)
+            
+            Spacer()
+        } //: HSTACK
+        .background(
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+        )
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
