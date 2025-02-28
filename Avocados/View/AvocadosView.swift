834412ca3225f0c703b2647044504b17ccbcce03
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-28 17:58:59 +0500

    Add the description to avocados viw work done

--- Avocados/View/AvocadosView.swift
+++ Avocados/View/AvocadosView.swift
@@ -18,14 +18,28 @@ struct AvocadosView: View {
                 .frame(width: 240, height: 240, alignment: .center)
                 .shadow(color: Color("ColorBlackTransparentDark"), radius: 12, x: 0, y: 8)
             
-            Text("Avocados")
-                .font(.system(size: 42, weight: .bold, design: .serif))
-                .foregroundColor(.white)
-                .padding()
-                .shadow(color: Color("ColorBlackTransparentDark"), radius: 4, x: 0, y: 4)
+            VStack {
+                Text("Avocados")
+                    .font(.system(size: 42, weight: .bold, design: .serif))
+                    .foregroundColor(.white)
+                    .padding()
+                    .shadow(color: Color("ColorBlackTransparentDark"), radius: 4, x: 0, y: 4)
+                
+                Text("""
+                Creamy, green, and full of nutrients!
+                Avocado is a powerhouse ingredient at any meal. Enjoy these handpicked avocado recipes for breakfast, lunch, dinner & more!
+                """)
+                .lineLimit(nil)
+                .font(.system(.headline, design: .serif))
+                .foregroundColor(Color("ColorGreenLight"))
+                .multilineTextAlignment(.center)
+                .lineSpacing(8)
+                .frame(maxWidth: 640, minHeight: 120)
+            } //: VSTACK
+            .padding()
             
             Spacer()
-        } //: HSTACK
+        } //: VSTACK
         .background(
             Image("background")
                 .resizable()
@@ -37,4 +51,5 @@ struct AvocadosView: View {
 
 #Preview {
     AvocadosView()
+        .environment(\.colorScheme, .dark)
 }
