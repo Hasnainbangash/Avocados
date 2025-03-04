05f5cb3870b63489d57a3c664d24381259223210
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-04 10:58:48 +0500

    Footer view wokr done for the reipes tab viw screen

--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Avocado Recipes")
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                
+                // MARK: - FOOTER
+                
+                VStack(alignment: .center, spacing: 20) {
+                    Text("All About Avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("ColorGreenAdaptive"))
+                        .padding(8)
+                    
+                    Text("Everything you wanted to know about avocados but were too afraid to ask.")
+                        .font(.system(.body, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.gray)
+                } //: VSTACK
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+            } //: VSTACK
+        } //: SCROLL
     }
 }
 
