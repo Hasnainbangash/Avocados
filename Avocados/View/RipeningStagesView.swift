10ed09fa1ac2fd7ef2df628173b6204fd15b6033
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-05 11:35:18 +0500

    Ripening satges view work done andworking well

--- Avocados/View/RipeningStagesView.swift
+++ Avocados/View/RipeningStagesView.swift
@@ -10,15 +10,30 @@ import SwiftUI
 struct RipeningStagesView: View {
     // MARK: - PROPERTIES
     
+    var ripeningStages: [Ripening] = ripeningData
+    
     // MARK: - BODY
     
     var body: some View {
-        Text("Ripening Stages")
+        ScrollView(.horizontal, showsIndicators: false) {
+            VStack {
+                Spacer()
+                HStack(alignment: .center, spacing: 25) {
+                    ForEach(ripeningStages) { item in
+                        RipeningView(ripening: item)
+                    } //: LOOP
+                } //: HSTACK
+                .padding(.vertical)
+                .padding(.horizontal, 25)
+                Spacer()
+            } //: VSTACK
+        } //: SCROLL
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
 // MARK: - PREVIEW
 
 #Preview {
-    RipeningStagesView()
+    RipeningStagesView(ripeningStages: ripeningData)
 }
