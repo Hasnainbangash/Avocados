d184e553e2ed715ab78d261e64ff58dbaf9191e8
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-05 11:46:37 +0500

    Sections commenst are added to th settings view

--- Avocados/View/SettingsView.swift
+++ Avocados/View/SettingsView.swift
@@ -8,10 +8,16 @@
 import SwiftUI
 
 struct SettingsView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
     var body: some View {
         Text("Settings")
     }
 }
+
+// MARK: - PREVIEW
 
 #Preview {
     SettingsView()
