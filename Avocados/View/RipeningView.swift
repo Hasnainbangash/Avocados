300d736f987b6aa7cc0c6f812327b564863d4d3f
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-05 10:40:33 +0500

    Ripenin view design work alomost done

--- Avocados/View/RipeningView.swift
+++ Avocados/View/RipeningView.swift
@@ -22,11 +22,67 @@ struct RipeningView: View {
             
             VStack(alignment: .center, spacing: 10) {
                 // STAGE
+                VStack(alignment: .center, spacing: 0) {
+                    Text("1")
+                        .font(.system(.largeTitle, design: .serif))
+                        .fontWeight(.bold)
+                    
+                    Text("STAGE")
+                        .font(.system(.body, design: .serif))
+                        .fontWeight(.heavy)
+                } //: VSTACK
+                .foregroundColor(Color("ColorGreenMedium"))
+                
                 // TITLE
+                Text("HARD")
+                    .font(.system(.title, design: .serif))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("ColorGreenMedium"))
+                    .padding(.vertical, 12)
+                    .padding(.horizontal, 0)
+                    .frame(width: 220)
+                    .background(
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(LinearGradient(gradient: Gradient(colors: [Color.white, Color("ColorGreenLight")]), startPoint: .top, endPoint: .bottom))
+                            .shadow(color: Color("ColorBlackTransparentLight"), radius: 6, x: 0, y: 6)
+                    )
+                
                 // DESCRIPTION
+                Text("Fresh off the tree, the avocado is ery hard with no give")
+                    .foregroundColor(Color("ColorGreenDark"))
+                    .fontWeight(.bold)
+                    .lineLimit(nil)
+                
                 // RIPENESS
+                Text("5+ DAYS")
+                    .foregroundColor(Color.white)
+                    .font(.system(.callout, design: .serif))
+                    .fontWeight(.bold)
+                    .shadow(radius: 3)
+                    .padding(.vertical)
+                    .padding(.horizontal, 0)
+                    .frame(width: 185)
+                    .background(
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(LinearGradient(gradient: Gradient(colors: [Color("ColorGreenMedium"), Color("ColorGreenDark")]), startPoint: .top, endPoint: .bottom))
+                            .shadow(color: Color("ColorBlackTransparentLight"), radius: 6, x: 0, y: 6)
+                    )
+                
                 // INSTRUCTION
-            }
+                Text("Hold avocados under room temperature unitl they are fully ripe.")
+                    .font(.footnote)
+                    .foregroundColor(Color("ColorGreenLight"))
+                    .fontWeight(.bold)
+                    .lineLimit(3)
+                    .frame(width: 160)
+                
+            } //: VSTACK
+            .zIndex(0)
+            .multilineTextAlignment(.center)
+            .padding(.horizontal)
+            .frame(width: 260, height: 485, alignment: .center)
+            .background(LinearGradient(gradient: Gradient(colors: [Color("ColorGreenLight"), Color("ColorGreenMedium")]), startPoint: .top, endPoint: .bottom))
+            .cornerRadius(20)
         } //: VSTACK
         .edgesIgnoringSafeArea(.all)
     }
